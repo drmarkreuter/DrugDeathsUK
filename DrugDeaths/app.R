bbe892886ad928a9d7aaaab80480ad993d36c40b
Mark Reuter <mark.reuter@googlemail.com>
2024-08-05 00:10:29 +0100

    adding App.R initial commit

---
+++ DrugDeaths/app.R
@@ -0,0 +1,242 @@
+#DrugDeaths App
+#A web application for examining drug-related deaths
+#Mark Reuter
+#August 2024
+#contact mark.reuter@googlemail.com
+#git
+#original data set from https://www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/deaths/datasets/deathsrelatedtodrugpoisoningbyselectedsubstances
+
+#Number of drug-related poisonings by selected substances, England and Wales, deaths registered between 1993 and 2022",,,,,,
+
+library(shiny)
+library(shinythemes)
+library(wesanderson)
+
+allData <- read.csv("data/2022substances.xlsx - All data.csv",
+                    header = TRUE)
+print(names(allData))
+
+allYears <- unique(allData$Year.of.death.registration)
+sexes <- unique(allData$Sex)
+residence <- unique(allData$Usual.residence.name)
+substance <- unique(allData$Substance)
+mentioned <- unique(allData$Mentioned)
+
+print(allYears)
+print(sexes)
+print(residence)
+print(substance)
+print(mentioned)
+
+
+#### UI ####
+ui <- fluidPage(
+
+    titlePanel("Drug Deaths"),
+
+    sidebarLayout(
+        sidebarPanel(
+          selectInput(
+            "year",
+            "Year",
+            allYears,
+            selected = "2022",
+            multiple = FALSE,
+            selectize = TRUE,
+            width = NULL,
+            size = NULL
+          ),
+          
+          selectInput(
+            "sex",
+            "Sex",
+            sexes,
+            selected = "Persons",
+            multiple = FALSE,
+            selectize = TRUE,
+            width = NULL,
+            size = NULL
+          ),
+          
+          selectInput(
+            "residence",
+            "Residence",
+            residence,
+            selected = "England and Wales",
+            multiple = FALSE,
+            selectize = TRUE,
+            width = NULL,
+            size = NULL
+          ),
+          
+          selectInput(
+            "substance",
+            "Substance",
+            substance,
+            selected = "01 All drug poisonings",
+            multiple = FALSE,
+            selectize = TRUE,
+            width = NULL,
+            size = NULL
+          ),
+          
+          actionButton("update",
+                       "Show/Update",
+                       width = 150),
+          
+          hr(),
+          HTML("<h3>Comparison with...</h3>"),
+          
+          selectInput(
+            "year2",
+            "Year",
+            allYears,
+            selected = "2022",
+            multiple = FALSE,
+            selectize = TRUE,
+            width = NULL,
+            size = NULL
+          ),
+          
+          selectInput(
+            "sex2",
+            "Sex",
+            sexes,
+            selected = "Persons",
+            multiple = FALSE,
+            selectize = TRUE,
+            width = NULL,
+            size = NULL
+          ),
+          
+          selectInput(
+            "residence2",
+            "Residence",
+            residence,
+            selected = "England and Wales",
+            multiple = FALSE,
+            selectize = TRUE,
+            width = NULL,
+            size = NULL
+          ),
+          
+          selectInput(
+            "substance2",
+            "Substance",
+            substance,
+            selected = "01 All drug poisonings",
+            multiple = FALSE,
+            selectize = TRUE,
+            width = NULL,
+            size = NULL
+          ),
+          
+          actionButton("update2",
+                       "Show/Update",
+                       width = 150)
+          
+        ),
+#### main panel ####
+        mainPanel(
+          HTML("<h4>Number of drug-related poisonings by selected substances, England and Wales, deaths registered between 1993 and 2022</h4>"),
+          dataTableOutput("filtered"),
+          plotOutput("plot"),
+          hr(),
+          plotOutput("plot2"),
+          plotOutput("plot3")
+        )
+    )
+)
+
+#### server ####
+server <- function(input, output) {
+
+    
+    observeEvent(input$update,{
+      filteredData <- allData
+      filteredData <- filteredData[filteredData$Year.of.death.registration==input$year,]
+      filteredData <- filteredData[filteredData$Sex==input$sex,]
+      filteredData <- filteredData[filteredData$Usual.residence.name==input$residence,]
+      filteredData <- filteredData[filteredData$Substance==input$substance,]
+      
+      output$filtered <- renderDT(
+        filteredData
+      )
+      
+      output$plot <- renderPlot({
+        barplot(filteredData$Deaths,
+                names.arg = filteredData$Mentioned,
+                ylab = "Deaths",
+                col=wes_palette(4,
+                                type = "continuous"),
+                main = paste0("Number of drug-related poisonings by selected substances, \n",input$residence,", deaths registered in ",
+                              input$year)
+                )
+      })
+      
+    })
+  
+  ##comparison
+  observeEvent(input$update2,{
+    
+    filteredData <- allData
+    filteredData <- filteredData[filteredData$Year.of.death.registration==input$year,]
+    filteredData <- filteredData[filteredData$Sex==input$sex,]
+    filteredData <- filteredData[filteredData$Usual.residence.name==input$residence,]
+    filteredData <- filteredData[filteredData$Substance==input$substance,]
+    
+    filteredData2 <- allData
+    filteredData2 <- filteredData2[filteredData2$Year.of.death.registration==input$year2,]
+    filteredData2 <- filteredData2[filteredData2$Sex==input$sex2,]
+    filteredData2 <- filteredData2[filteredData2$Usual.residence.name==input$residence2,]
+    filteredData2 <- filteredData2[filteredData2$Substance==input$substance2,]
+    
+    output$plot2 <- renderPlot({
+      barplot(filteredData2$Deaths,
+              names.arg = filteredData2$Mentioned,
+              ylab = "Deaths",
+              col=wes_palette(4,
+                              type = "continuous"),
+              main = paste0("Number of drug-related poisonings by selected substances, \n",input$residence2,", deaths registered in ",
+                            input$year2)
+      )
+    })
+    
+    comp.matrix <- t(as.matrix(data.frame(A=filteredData$Deaths,
+                                        B=filteredData2$Deaths)))
+    
+    #rownames(comp.matrix) <- c("On the death certificate", "Without other drugs","With alcohol","Without alcohol")
+    #rownames(comp.matrix) <- c("On the death certificate", "Without other drugs","With alcohol","Without alcohol")
+    colnames(comp.matrix) <- c("On the death certificate", "Without other drugs","With alcohol","Without alcohol")
+    print(comp.matrix)
+    output$plot3 <- renderPlot({
+      barplot(comp.matrix,
+              #names.arg = row.names(comp.matrix),
+              col = c("red", "blue"),
+              beside = TRUE,
+              ylab = "Deaths")
+      legend("topright",                                    
+             legend = c(
+               paste0(
+                input$year," | ",
+                input$sex," | ",
+                input$residence," | ",
+                input$substance),
+               paste0(
+                 input$year2," | ",
+                 input$sex2," | ",
+                 input$residence2," | ",
+                 input$substance2
+               )),
+             fill = c("red", "blue"))
+    })
+    
+  })
+    
+  
+  
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
